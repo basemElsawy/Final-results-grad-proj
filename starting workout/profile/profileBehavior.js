000d29e51cf6c79edafb0c50a7b430ad1b8c576d
Basem Mohamed <besoelpop2@gmail.com>
2022-12-23 03:11:53 +0200

    profile behavior part 2

--- starting workout/profile/profileBehavior.js
+++ starting workout/profile/profileBehavior.js
@@ -7,9 +7,10 @@ const precentages = document.querySelectorAll('.precentage');
 
 
 //-=-----------------------========== second section variables ===-------
-const secondBodyContainer = document.querySelector('.body-container');
+const secondBodyContainer = document.querySelector('#description');
 const editBtn = document.querySelectorAll('.edit-btn');
-
+const infoHeadLn = document.querySelectorAll('.info-headline');
+const infoArticle = document.querySelectorAll('.info-article')
 
 
 //-----------============= first section variables ===========---------
@@ -17,7 +18,8 @@ const changeBtn = document.querySelectorAll('.change-btn');
 const personalData = document.querySelectorAll('.personal-data');
 const infoContainer = document.querySelector('.info-container');
 const infoBody = document.querySelector('.info-body');
-
+const bodyContainer = document.querySelector('.body-container');
+const addMore = document.querySelector('.Add-more');
 
 
 
@@ -32,6 +34,187 @@ const names = document.querySelectorAll('.name')
 // ----------------========== global variables ===========-------------
 var message = document.createElement('p');
 
+
+
+//-----------------------------=============ADD MORE FUNMCTIONALITY ===========---------
+
+addMore.addEventListener('click', () => {
+    let bodyContainerNewDiv = document.createElement('div');
+    bodyContainerNewDiv.classList.add('info');
+    bodyContainer.append(bodyContainerNewDiv);
+    let insideLabel = document.createElement('label');
+    let secondLabel = document.createElement('label');
+    let newInputEl = document.createElement('input');
+    insideLabel.setAttribute('for', 'newInputEl')
+    newInputEl.setAttribute('id', 'newInputEl');
+    secondLabel.setAttribute('for', 'text-area');
+    secondLabel.textContent = 'whats on your mind';
+    let textAreaEl = document.createElement('textarea');
+    textAreaEl.setAttribute('id', 'text-area');
+    textAreaEl.style.width = '200px'
+    textAreaEl.style.height = '100px'
+    document.querySelector('.body-container').style.filter = 'blur(10px)';
+    insideLabel.textContent = 'Description header'
+    insideLabel.style.fontWeight = 'bold';
+
+    let thirdNewDiv = document.createElement('div');
+    thirdNewDiv.classList.add('second-container-newDiv');
+    secondBodyContainer.append(thirdNewDiv);
+    thirdNewDiv.append(insideLabel);
+    thirdNewDiv.append(newInputEl);
+    thirdNewDiv.append(secondLabel);
+    thirdNewDiv.append(textAreaEl);
+
+    let submitBtn = document.createElement('button');
+    submitBtn.classList.add('submit-text');
+    submitBtn.textContent = 'Submit';
+
+    thirdNewDiv.append(submitBtn);
+    document.querySelector('.body-container').style.filter = 'blur(10px)';
+    let newHeadLn = document.createElement('h3');
+    newHeadLn.classList.add('info-headline');
+    let newArticle = document.createElement('p');
+    newArticle.classList.add('infoArticle')
+    let newEditBtn = document.createElement('button');
+    newEditBtn.classList.add('edit-btn');
+    newEditBtn.textContent = 'Edit';
+    bodyContainerNewDiv.append(newHeadLn);
+    bodyContainerNewDiv.append(newArticle);
+    bodyContainerNewDiv.append(newEditBtn);
+    editBtn.forEach((btn, idx) => {
+        btn.addEventListener('click', () => {
+            let secondNewDiv = document.createElement('div');
+            secondNewDiv.classList.add('second-container-newDiv');
+            document.getElementById('select-two').classList.add('over-lay')
+            let insideLabel = document.createElement('label');
+            let secondLabel = document.createElement('label');
+            insideLabel.textContent = 'Description header'
+            insideLabel.style.fontWeight = 'bold';
+
+            let newInputEl = document.createElement('input');
+            insideLabel.setAttribute('for', 'newInputEl')
+            newInputEl.setAttribute('id', 'newInputEl');
+            secondLabel.setAttribute('for', 'text-area');
+            secondLabel.textContent = 'whats on your mind';
+            let textAreaEl = document.createElement('textarea');
+            textAreaEl.setAttribute('id', 'text-area');
+            textAreaEl.style.width = '200px'
+            textAreaEl.style.height = '100px'
+
+            let submitBtn = document.createElement('button');
+            submitBtn.classList.add('submit-text');
+            submitBtn.textContent = 'Submit';
+
+            document.querySelector('.body-container').style.filter = 'blur(10px)';
+            secondNewDiv.style.zIndex = '100';
+            secondBodyContainer.append(secondNewDiv);
+            secondNewDiv.append(insideLabel);
+            secondNewDiv.append(newInputEl);
+            secondNewDiv.append(secondLabel);
+            secondNewDiv.append(textAreaEl);
+            secondNewDiv.append(submitBtn);
+            submitBtn.addEventListener('click', () => {
+                if (newInputEl.value && textAreaEl.value) {
+                    infoHeadLn[idx].textContent = newInputEl.value
+                    infoArticle[idx].textContent = textAreaEl.value
+                    submitBtn.parentElement.remove();
+                    document.querySelector('#select-two').classList.remove('over-lay');
+                    document.querySelector('.body-container').style.filter = 'blur(0px)';
+
+                }
+                else {
+
+                    message.textContent = 'please enter your new details';
+
+                    secondNewDiv.append(message)
+
+                }
+            })
+
+        })
+    })
+    submitBtn.addEventListener('click', () => {
+
+        if (newInputEl.value && textAreaEl.value) {
+            newHeadLn.textContent = newInputEl.value
+            newArticle.textContent = textAreaEl.value
+            submitBtn.parentElement.remove();
+            document.querySelector('#select-two').classList.remove('over-lay');
+            document.querySelector('.body-container').style.filter = 'blur(0px)';
+
+        }
+        else {
+
+            message.textContent = 'please enter your new details';
+
+            thirdNewDiv.append(message)
+
+        }
+    })
+
+
+
+})
+
+
+// ------------------========= second container functionality =========-=----------
+editBtn.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        let secondNewDiv = document.createElement('div');
+        secondNewDiv.classList.add('second-container-newDiv');
+        document.getElementById('select-two').classList.add('over-lay')
+        let insideLabel = document.createElement('label');
+        let secondLabel = document.createElement('label');
+        insideLabel.textContent = 'Description header'
+        insideLabel.style.fontWeight = 'bold';
+
+        let newInputEl = document.createElement('input');
+        insideLabel.setAttribute('for', 'newInputEl')
+        newInputEl.setAttribute('id', 'newInputEl');
+        secondLabel.setAttribute('for', 'text-area');
+        secondLabel.textContent = 'whats on your mind';
+        let textAreaEl = document.createElement('textarea');
+        textAreaEl.setAttribute('id', 'text-area');
+        textAreaEl.style.width = '200px'
+        textAreaEl.style.height = '100px'
+
+        let submitBtn = document.createElement('button');
+        submitBtn.classList.add('submit-text');
+        submitBtn.textContent = 'Submit';
+
+        document.querySelector('.body-container').style.filter = 'blur(10px)';
+        secondNewDiv.style.zIndex = '100';
+        secondBodyContainer.append(secondNewDiv);
+        secondNewDiv.append(insideLabel);
+        secondNewDiv.append(newInputEl);
+        secondNewDiv.append(secondLabel);
+        secondNewDiv.append(textAreaEl);
+        secondNewDiv.append(submitBtn);
+        submitBtn.addEventListener('click', () => {
+            if (newInputEl.value && textAreaEl.value) {
+                infoHeadLn[idx].textContent = newInputEl.value
+                infoArticle[idx].textContent = textAreaEl.value
+                submitBtn.parentElement.remove();
+                document.querySelector('#select-two').classList.remove('over-lay');
+                document.querySelector('.body-container').style.filter = 'blur(0px)';
+
+            }
+            else {
+
+                message.textContent = 'please enter your new details';
+
+                secondNewDiv.append(message)
+
+            }
+        })
+
+    })
+})
+
+
+
+
+
 //----------------============change-details-btn=========---------
 
 changeBtn.forEach((btn, idx) => {
@@ -43,7 +226,7 @@ changeBtn.forEach((btn, idx) => {
         let newInput = document.createElement('input');
         newInput.setAttribute('id', 'new-input');
         let labelInput = document.createElement('label');
-        labelInput.textContent = 'New Data';
+        labelInput.textContent = 'New personal info';
         labelInput.setAttribute('for', 'new-input');
         let submitBtn = document.createElement('button');
         submitBtn.classList.add('submit-text');
@@ -53,6 +236,12 @@ changeBtn.forEach((btn, idx) => {
         newDiv.append(submitBtn);
         infoBody.style.filter = 'blur(10px)';
         newDiv.style.borderRadius = '5px'
+        document.getElementById('select-one').classList.add('over-lay');
+        // for (let i = 0; i < changeBtn.length; i++) {
+        //     changeBtn[i].classList.add('hide');
+
+        // }
+        newDiv.style.zIndex = '100';
 
 
         if (personalData[idx].classList.contains('Age-data') === true) {
@@ -72,6 +261,12 @@ changeBtn.forEach((btn, idx) => {
                 names[idx].textContent = newInput.value
                 submitBtn.parentElement.remove();
                 infoBody.style.filter = 'blur(0px)';
+                // for (let i = 0; i < changeBtn.length; i++) {
+                //     changeBtn[i].classList.remove('hide');
+
+                // }
+                document.getElementById('select-one').classList.remove('over-lay');
+
             }
 
             else {
