65febaa604df7fcb34002dc1735d28464e2a0842
Basem Mohamed <besoelpop2@gmail.com>
2022-12-30 01:59:35 +0200

    new assessment page  and homepage design part 4

--- assessment page/assessingBehavior.js
+++ assessment page/assessingBehavior.js
@@ -16,150 +16,51 @@ let navbar = document.querySelector('.navbar');
 let logoImg = document.querySelector('.muscle-img');
 
 //----------=========== new elements =======----------
-const formContainer = document.querySelectorAll('.form-container');
-const formHeader = document.querySelectorAll('.form-header');
-const passwordInput = document.getElementById('psw');
-let letter = document.getElementById('letter');
-let capital = document.getElementById('capital');
-let number = document.getElementById('number');
-let length = document.getElementById('length');
-const imageInput = document.querySelector('#input-image');
-const imageInside = document.querySelector('.image-unknown');
-var uploadedImage = '';
+const sections = document.querySelectorAll('.sect-behavior');
+const sectionsContainer = document.querySelector('.sections-container');
+const footerSection = document.querySelector('.footer-section');
+const startBtn = document.querySelector('.start');
+const frstFormSect = document.querySelector('.third-section');
+const secondSection = document.getElementById('second-section');
+const navigationBar = document.querySelector('.side-bar');
+
+
+
 // -------------======== Variables =======---------------
 
+// -----------============== Forms initiation ========-------------
+
+
+startBtn.addEventListener('click', () => {
+  frstFormSect.style.display = 'block';
+  footerSection.classList.remove('hidden');
+
+  secondSection.style.opacity = '1';
+  navigationBar.style.opacity = '1';
+  startBtn.style.display = 'none';
+
+  navigationBar.style.display = 'block';
+  setTimeout(() => {
+    frstFormSect.style.transform = 'translateY(0px)'
+    frstFormSect.style.height = '100%';
+    frstFormSect.style.opacity = '1';
+    frstFormSect.style.zIndex = '1';
+  }, 500)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 // -------------================= Event LISTENERS ===========--------------
-imageInput.addEventListener('change', (e) => {
-  const imageIndex = imageInput.files[0];
-  const reader = new FileReader();
-  imageInside.removeAttribute('src');
-  reader.addEventListener('load', () => {
-    uploadedImage = reader.result;
-    imageInside.setAttribute('src', uploadedImage);
-  });
 
-  reader.readAsDataURL(imageIndex);
-});
-
-passwordInput.onfocus = () => {
-  document.getElementById('message').style.display = 'block';
-};
-passwordInput.onblur = () => {
-  document.getElementById('message').style.display = 'none';
-};
-
-passwordInput.onkeyup = () => {
-  var lowerCaseLetters = /[a-z]/g;
-  var upperCaseLetters = /[A-Z]/g;
-  var numbers = /[0-9]/g;
-  if (passwordInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove('invalid');
-    letter.classList.add('valid');
-  } else {
-    letter.classList.remove('valid');
-    letter.classList.add('invalid');
-  }
-  if (passwordInput.value.match(upperCaseLetters)) {
-    capital.classList.remove('invalid');
-    capital.classList.add('valid');
-  } else {
-    capital.classList.remove('valid');
-    capital.classList.add('invalid');
-  }
-  if (passwordInput.value.match(numbers)) {
-    number.classList.remove('invalid');
-    number.classList.add('valid');
-  } else {
-    number.classList.remove('valid');
-    number.classList.add('invalid');
-  }
-  if (passwordInput.value.length >= 8) {
-    length.classList.remove('invalid');
-    length.classList.add('valid');
-  } else {
-    length.classList.remove('valid');
-    length.classList.add('invalid');
-  }
-};
-
-formContainer.forEach((contain, idx) => {
-  contain.addEventListener('mouseover', () => {
-    formHeader[idx].style.backgroundColor = '#223341';
-  });
-});
-formContainer.forEach((contain, idx) => {
-  contain.addEventListener('mouseout', () => {
-    formHeader[idx].style.backgroundColor = '#22334177';
-  });
-});
-// height.value = Number(height.value);
-
-// weight.value = Number(weight.value);
-
-// age.value = Number(age.value);
-// submitBtn.addEventListener('click', function () {
-//   let bmi;
-//   let bmr;
-//   let results = ``;
-//   if (gender.value === 'male') {
-//     bmi = weight.value / height.value ** 2;
-
-//     bmi = parseFloat(Number(bmi)).toFixed(1);
-//     let weightCalc = weight.value * 10;
-//     let heightCalc = height.value * 6.25;
-//     heightCalc = heightCalc * 100;
-//     let ageCalc = age.value * 5;
-
-//     bmr = Number(weightCalc) + Number(heightCalc) - Number(ageCalc) + 5;
-//     bmr = parseFloat(Number(bmr)).toFixed(1);
-//     console.log(bmr);
-//     if (bmi <= 18.5) {
-//       results = `Hello ${firstName.value} ${lastName.value} Your body mass index is ${bmi} and you're slightly underweight also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 18.5 && bmi <= 24.9) {
-//       results = `Hello ${firstName.value} ${lastName.value} Your body mass index is ${bmi} and your weight is normal also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 25 && bmi <= 29.9) {
-//       results = `Hello ${firstName.value} ${lastName.value}  Your body mass index is ${bmi} and you're slightly overweight also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 30) {
-//       results = `Hello ${firstName.value} ${lastName.value}  Your body mass index is ${bmi} and you're suffering obesity also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-
-//     document.getElementById('results').append(results);
-//     document.getElementById('results-container').classList.remove('hidden');
-//     document.querySelector('.Next-page').classList.remove('hidden');
-//     document.querySelector('.submit').classList.add('hidden');
-//     refreshBtn.classList.remove('hidden');
-//   } else if (gender.value === 'female') {
-//     bmi = weight.value / height.value ** 2;
-//     bmi = parseFloat(Number(bmi)).toFixed(1);
-//     let weightCalc = weight.value * 10;
-//     let heightCalc = height.value * 6.25;
-//     heightCalc = heightCalc * 100;
-//     let ageCalc = age.value * 5;
-
-//     bmr = Number(weightCalc) + Number(heightCalc) - Number(ageCalc) - 161;
-//     bmr = parseFloat(Number(bmr)).toFixed(1);
-//     if (bmi <= 18.5) {
-//       results = `Hello ${firstName.value} ${lastName.value} Your body mass index is ${bmi} and you're slightly underweight also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 18.5 && bmi <= 24.9) {
-//       results = `Hello ${firstName.value} ${lastName.value} Your body mass index is ${bmi} and your weight is normal also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 25 && bmi <= 29.9) {
-//       results = `Hello ${firstName.value} ${lastName.value}  Your body mass index is ${bmi} and you're slightly overweight also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     if (bmi >= 30) {
-//       results = `Hello ${firstName.value} ${lastName.value}  Your body mass index is ${bmi} and you're suffering obesity also your basal metabolic rate is ${bmr} keep that in mind cause it'll help alot with the diet plan soon on our website.`;
-//     }
-//     document.getElementById('results').append(results);
-//     document.getElementById('results-container').classList.remove('hidden');
-//     document.querySelector('.Next-page').classList.remove('hidden');
-//     document.querySelector('.submit').classList.add('hidden');
-//     refreshBtn.classList.remove('hidden');
-//   }
-// });
 
 document.addEventListener('DOMContentLoaded', function (event) {
   window.onscroll = function () {
@@ -168,14 +69,26 @@ document.addEventListener('DOMContentLoaded', function (event) {
 });
 
 let scrollFunction = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 350) {
+    document.querySelector('.muscle-img').style.opacity = '1';
+    header.style.backgroundColor = '#223341'
     header.style.boxShadow = '0px 2px 40px 2px black';
+
+  }
+  else {
+    document.querySelector('.muscle-img').style.opacity = '0';
+    header.style.backgroundColor = 'transparent'
+    header.style.boxShadow = 'none';
+
+  }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // header.style.boxShadow = '0px 2px 40px 2px black';
     logo.style.transform = 'scale(1)';
     navbar.style.transform = 'scale(1)';
   } else {
     navbar.style.transform = 'scale(1.2)';
     logo.style.transform = 'scale(1.2)';
-    header.style.boxShadow = 'none';
+    // header.style.boxShadow = 'none';
   }
   if (
     document.body.scrollTop > 600 ||
